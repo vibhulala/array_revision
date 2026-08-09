ed114be216b32a9998e8b63d11b6b8f2c4604cd1
vibhulala <bitwisenilay2006@gmail.com>
2026-08-10 02:40:39 +0530

    Time: 7 ms (52.93%), Space: 26.3 MB (93.63%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.py
+++ 0189-rotate-array/0189-rotate-array.py
@@ -1,8 +1,26 @@
 class Solution:
     def rotate(self, nums: List[int], k: int) -> None:
+        n = len(nums)
+        k = k % n
+
+        def reverse(nums, left, right):
+            while left < right:
+                nums[left], nums[right] = nums[right], nums[left]
+                left += 1
+                right -= 1
+
+        reverse(nums, n-k, n-1)
+        reverse(nums, 0, n-k-1)
+        reverse(nums, 0, n-1)
         """
         Do not return anything, modify nums in-place instead.
         """
+        '''
+        here wqe are using the slicing method but for other languages we mnust know the other approach which will comes under optimized approach 
         n=len(nums)
         k=k%n
         nums[:]=nums[n-k:]+nums[:n-k]
+        '''
+    
+
+            

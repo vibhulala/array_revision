b1896317c278846f02044541ff8fb1a090e93df3
vibhulala <bitwisenilay2006@gmail.com>
2026-08-11 02:25:17 +0530

    Time: 15 ms (26.13%), Space: 35.8 MB (19.26%) - LeetHub

---
+++ 0974-subarray-sums-divisible-by-k/0974-subarray-sums-divisible-by-k.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    int subarraysDivByK(vector<int>& nums, int k) {
+        /*
+        int n=nums.size();
+        int result=0;
+        //cummulative sum
+        for (int i=0;i<n;i++){
+            nums[i]+=nums[i-1];
+        }
+        for (int i=0;i<n;i++){
+            for (int j=i;j<n;j++){
+                int sum=(i==0)?nums[j]:nums[j]-nums[i-1];
+                if (sum%k==0)  
+                   result++;
+            }
+        }
+        return result;
+        */
+        int n=nums.size();
+        unordered_map<int,int> mp;
+        int sum=0;
+        mp[0]=1;
+        int result=0;
+        for (int i=0;i<n;i++){
+            sum+=nums[i];
+            int rem =sum%k;
+            if(rem<0){
+                rem+=k;
+            }
+            if (mp.find(rem)!=mp.end()){
+                result+=mp[rem];
+            }
+            mp[rem]++;
+        }
+        return result;
+    }
+};
